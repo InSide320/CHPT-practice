c0b33523357a08a319f194b02c78fcf656d444fc
Denis Kud <dekud2109@gmail.com>
2021-11-26 10:19:10 +0200

    Add new icon Add MenuBar in GUIForm.java

--- GUIForm/src/GUIForm.java
+++ GUIForm/src/GUIForm.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 
 public class GUIForm extends JFrame {
@@ -8,6 +10,16 @@ public class GUIForm extends JFrame {
     GUIForm(String response, String color, String path) {
         super("Response from server");
         this.setResponse(response);
+        URL imgURLLogo = GUIForm.class.getResource("resource/iconfinder_logo.png");
+        ImageIcon iconLogo = new ImageIcon(imgURLLogo);
+        JLabel labelNameCompany = new JLabel("CMETAHA");
+        JLabel labelLogo = new JLabel(iconLogo);
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu menu = new JMenu("О программе");
+        menuBar.add(menu);
+        JMenuItem Author = new JMenuItem("О разработчике");
+        menu.add(Author);
         JLabel label = new JLabel(getResponse());
         URL imgURL = GUIForm.class.getResource(path);
         ImageIcon icon = new ImageIcon(imgURL);
@@ -19,14 +31,23 @@ public class GUIForm extends JFrame {
             label.setForeground(Color.BLUE);
         } else
             label.setForeground(Color.BLACK);
+        labelLogo.setIcon(iconLogo);
         labelicon.setIcon(icon);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(700, 700);
         setLayout(new GridLayout(2, 2, 2, 2));
+        getContentPane().add(labelNameCompany);
+        getContentPane().add(labelLogo);
         getContentPane().add(label); // add new component
         getContentPane().add(labelicon);
         pack();
         setVisible(true);
+        Author.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null ,"Author: Denis Kud");
+            }
+        });
     }
 
     public String getResponse() {
